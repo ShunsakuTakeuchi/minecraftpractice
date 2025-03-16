a5a7a45c3bf3f79ca03828568a961b6f281907f4
ShunsakuTakeuchi <takeuchi.s.ai@m.titech.ac.jp>
2025-03-16 12:45:51 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% color=#0fbc11 icon="\uf120" weight=100
 namespace MyExtension {
-    //% block="Hello world"
-    export function helloWorld() {
-        console.log("Hello World!")
+    //% block="placeblockpra"
+    export function placeblockpra(){
+        blocks.place(GRASS, pos(0, 0, 0))
     }
 }
